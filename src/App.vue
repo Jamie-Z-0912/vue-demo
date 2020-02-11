e08f78c022cb67883c8ea063633b610718f30536
Jamie <>
2020-02-11 20:46:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <div :id="message">{{message}}</div>
+    <TodoList>
+      <TodoItem :key="item.title" @delete="handleDelete" v-for="item in list" :del="item.del" :title="item.title">
+        <template v-slot:pre-icon="{value}">
+          <span>前置图标{{item.title}}{{value}}</span>
+        </template>
+      </TodoItem>
+    </TodoList>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList';
+import TodoItem from './components/TodoItem';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    TodoItem,
+  },
+  data () {
+    return {
+      message: 'Hello World',
+      list: [
+        {
+          title: '课程1',
+          del: false,
+        },
+        {
+          title: '课程2',
+          del: false,
+        },
+      ]
+    }
+  },
+  methods: {
+    handleDelete (val) {
+      console.log('handleDelete', val);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <slot name="pre" :value="value"></slot>
+    <span v-if="!del">{{title}}</span>
+    <span v-else style="text-decoration:line-throw">{{title}}</span>
+    <slot name="sub-icon">😄</slot>
+    <button v-show="!del" @click="handleClick">删除</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    del: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  data: function () {
+    return {
+      value: Math.random()
+    }
+  },
+  methods: {
+    handleClick () {
+      console.log('点击删除按钮');
+      this.$emit('delete', this.title)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
